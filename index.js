0f9cc45e16d6cc1f3b1ab097f46100027ca29e73
python-programing <79223364+python-coding-404@users.noreply.github.com>
2022-02-25 08:51:34 +0000

    hello

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ let express = require("express")
 let expresssession = require("express-session")
 
 let app = express();
-app.use(cookieParser('keyboard cat'));
-app.use(expresssession({ cookie: { maxAge: 9999999999 }, secret: "8bbfc54a5d6b345f89e048ee1430768decfa483f"}));
+app.set 
+app.use(cookieParser('8bbfc54a5d6b345f89e048ee1430768decfa483f'));
+app.use(expresssession({ cookie: { maxAge: 9999999999 }, secret: "8bbfc54a5d6b345f89e048ee1430768decfa483f", resave: true,
+	saveUninitialized: true}));
 app.use(flash());
+app.set("view engine", "ejs")
+app.all('/express-flash',function(req,res){
+  req.flash('success', 'This is a flash message using the express-flash module.');
+  res.redirect(301, '/');
+})
 
-app.get('/', (req, res) => {
-  res.send('Hello Express app!')
-});
+app.get('/', req, res ) {
+  res.render('index', {expressFlash: req.flash('success') });
+}
 
 app.listen(3000, () => {
   console.log('server started');
